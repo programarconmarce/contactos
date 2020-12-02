57430c6f348d4f88045e3247c2b998cb6f16a5b1
programarconmarce <75289356+programarconmarce@users.noreply.github.com>
2020-12-02 04:51:38 -0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -6,4 +6,4 @@ class RegistrationForm(Form):
     fullname = StringField('Fullname', [validators.DataRequired()])
     phone = StringField('Phone', [validators.DataRequired()])
     email = StringField('Email Address', [validators.DataRequired()])
-    #accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    
