61c2cabdf7b21163e68ae35ac46a423956ea314b
diego <odiegor052@gmail.com>
2020-12-02 14:32:43 -0300

    abm con json

--- app.py
+++ app.py
@@ -37,29 +37,6 @@ def add_contact():
         flash ('Contact Added successfully')
     return redirect(url_for('index'))
 
-@app.route('/create_Contact', methods=['POST'])
-def create_Contact():
-
-    fullname = request.json['fullname']
-    phone = request.json ['phone']
-    email = request.json ['email']
-
-    ction = mysql.connect()
-    cur = ction.cursor()
-    cur.execute('INSERT INTO contact (fullname,phone,email) VALUES(%s,%s,%s)',(fullname,phone,email))
-    ction.commit()
-    
-    return jsonify({"menssage":"Contacto agregado satisfactoriamente"})
-
-@app.route('/list_contact',methods=['GET'])
-def get_list():
-    ction = mysql.connect()
-    cur = ction.cursor()
-    cur.execute('SELECT * FROM CONTACT')
-    data = cur.fetchall()
-    #print (data)
-    return jsonify(contacts=data)  
-   
 @app.route('/edit/<id>')
 def get_contact(id):
     ction = mysql.connect()
@@ -90,8 +67,63 @@ def delete_contact(id):
     cur.execute('DELETE FROM CONTACT WHERE id = {0}'.format(id))
     ction.commit()
     flash('Contact Removed Successfully')
-    return redirect(url_for('index'))    
+    return redirect(url_for('index'))  
+
+# Funciones en formato json para manejar desde Postman  
+
+@app.route('/create_Contact', methods=['POST'])
+def create_Contact():
+
+    fullname = request.json['fullname']
+    phone = request.json ['phone']
+    email = request.json ['email']
+
+    ction = mysql.connect()
+    cur = ction.cursor()
+    cur.execute('INSERT INTO contact (fullname,phone,email) VALUES(%s,%s,%s)',(fullname,phone,email))
+    ction.commit()
+    
+    return jsonify({"menssage":"Contacto agregado satisfactoriamente"})
+
+@app.route('/list_contact',methods=['GET'])
+def get_list():
+    ction = mysql.connect()
+    cur = ction.cursor()
+    cur.execute('SELECT * FROM CONTACT')
+    data = cur.fetchall()
+    
+    return jsonify(contacts=data) 
+
+@app.route('/contact/<id>',methods=['GET'])
+def contact(id):   
+    ction = mysql.connect()
+    cur = ction.cursor()
+    cur.execute('SELECT * FROM CONTACT WHERE id = %s',(id))
+    data = cur.fetchall()
+    
+    return jsonify(contact=data[0]) 
+
+@app.route('/update/<id>',methods=['PUT']) 
+def update(id):
+    
+    fullname = request.json ['fullname']
+    phone = request.json ['phone']
+    email = request.json ['email']
+    ction = mysql.connect()
+    cur = ction.cursor()
+    cur.execute ("UPDATE CONTACT  SET fullname=%s,email=%s, phone=%s  WHERE id=%s " ,(fullname,email,phone,id))
+    ction.commit()
+    return  jsonify({"menssage":"Contacto modificado satisfactoriamente"})
+
+@app.route('/remove/<string:id>',methods=['DELETE'])
+def remove(id):
+    ction = mysql.connect()
+    cur = ction.cursor()
+    cur.execute('DELETE FROM CONTACT WHERE id = {0}'.format(id))
+    ction.commit()
+    
+    return  jsonify({"menssage":"Contacto eliminado satisfactoriamente"})
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
      
