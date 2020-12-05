5b712e3de94ef97a7e3f644262edefb95597cdd7
programarconmarce <75289356+programarconmarce@users.noreply.github.com>
2020-12-04 23:10:10 -0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from wtforms import Form, BooleanField, StringField,validators
 
 
-
+#######FORMULARIO RENDERIZADO######
 class RegistrationForm(Form):
     fullname = StringField('Fullname', [validators.DataRequired()])
     phone = StringField('Phone', [validators.DataRequired()])
